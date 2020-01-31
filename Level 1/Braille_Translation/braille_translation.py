605cec9b7ca6a00746be7dab8aa63d6bf9eb2cb9
Peter Cao <peterspellingbee@gmail.com>
2020-01-31 22:23:16 +0000

    Started Level 2

--- Level 1/Braille_Translation/braille_translation.py
+++ Level 1/Braille_Translation/braille_translation.py
@@ -1,11 +1,9 @@
 line="011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110"
 braille_key = [line[i:i+6] for i in range(0, len(line), 6)]
 
-string = "The quick brown fox jumps over the lazy dog"
+string = "the quick brown fox jumps over the lazy dog"
 
-lower_string = string.lower()
-
-letter_dict={lower_string[x]:braille_key[x] for x in range(len(braille_key))}
+letter_dict={string[x]:braille_key[x] for x in range(len(braille_key))}
 
 def solution(s):
   braille = ""
