97403e794cc63ba26ec1bd1abf00ca1fdce88cdb
Peter Cao <peterspellingbee@gmail.com>
2020-01-31 20:41:57 +0000

    Initial commit

---
+++ braille_test.py
@@ -0,0 +1,16 @@
+import unittest
+from braille_translation import solution
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_Braille(self):
+      self.assertEqual(solution("Braille"), "000001110000111010100000010100111000111000100010")
+
+  def test_code(self):
+      self.assertEqual(solution("code"),"100100101010100110100010")
+
+  def test_long(self):
+      self.assertEqual(solution("The quick brown fox jumps over the lazy dog"),"000001011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110")
+
+if __name__ == '__main__':
+  unittest.main()
---
+++ braille_translation.py
@@ -0,0 +1,18 @@
+line="011110110010100010000000111110101001010100100100101000000000110000111010101010010111101110000000110100101010101101000000010110101001101100111100011100000000101010111001100010111010000000011110110010100010000000111000100000101011101111000000100110101010110110"
+braille_key = [line[i:i+6] for i in range(0, len(line), 6)]
+
+string = "The quick brown fox jumps over the lazy dog"
+
+lower_string = string.lower()
+
+letter_dict={lower_string[x]:braille_key[x] for x in range(len(braille_key))}
+
+def solution(s):
+  braille = ""
+  for letter in s:
+    if letter in letter_dict.keys():
+      braille+=letter_dict[letter]
+    else:
+      braille+="000001"
+      braille+=letter_dict[letter.lower()]
+  return braille
